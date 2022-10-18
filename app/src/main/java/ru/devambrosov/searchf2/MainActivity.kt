3e71ffd6c9bd7d58b103224d9bc57bbbdf4ae80c
olegAmbr <92858574+olegAmbr@users.noreply.github.com>
2022-10-18 15:19:30 +0400

    2 commit

--- app/src/main/java/ru/devambrosov/searchf2/MainActivity.kt
+++ app/src/main/java/ru/devambrosov/searchf2/MainActivity.kt
@@ -2,10 +2,30 @@ package ru.devambrosov.searchf2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initButtons()
+    }
+
+    private fun initButtons() {
+        button1.SetOnClickListener() {
+             Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        }
+        button2.SetOnClickListener() {
+            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+        }
+        button3.SetOnClickListener() {
+            Toast.makeText(this, "Категории", Toast.LENGTH_SHORT).show()
+        }
+        button4.SetOnClickListener() {
+            Toast.makeText(this, "Продолжить просмотр", Toast.LENGTH_SHORT).show()
+        }
+        button5.SetOnClickListener() {
+            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+        }
     }
 }
