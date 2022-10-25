3fd080396763820d72de577065da8071ddfbe41d
olegAmbr <92858574+olegAmbr@users.noreply.github.com>
2022-10-25 11:45:53 +0400

    4 commit

--- app/src/main/java/ru/devambrosov/searchf2/MainActivity.kt
+++ app/src/main/java/ru/devambrosov/searchf2/MainActivity.kt
@@ -1,34 +1,33 @@
 package ru.devambrosov.searchf2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.appcompat.app.AppCompatActivity
 import ru.devambrosov.searchf2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding = ActivityMainBinding.inflate(layoutInflater)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         initButtons()
     }
 
 
     private fun initButtons() {
-        button1.SetOnClickListener {
+        binding.button1.setOnClickListener {
              Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
         }
-        button2.SetOnClickListener {
+        binding.button2.setOnClickListener {
             Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
         }
-        button3.SetOnClickListener {
+        binding.button3.setOnClickListener {
             Toast.makeText(this, "Категории", Toast.LENGTH_SHORT).show()
         }
-        button4.SetOnClickListener {
+        binding.button4.setOnClickListener {
             Toast.makeText(this, "Продолжить просмотр", Toast.LENGTH_SHORT).show()
         }
-        button5.SetOnClickListener {
+        binding.button5.setOnClickListener {
             Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
         }
 
