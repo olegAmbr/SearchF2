7aad736746ad32dbc31a5c6958aaaf19338c6eb9
olegAmbr <92858574+olegAmbr@users.noreply.github.com>
2022-11-21 22:58:42 +0400

    module_24

--- app/src/main/java/ru/devambrosov/searchf2/FilmViewHolder.kt
+++ app/src/main/java/ru/devambrosov/searchf2/FilmViewHolder.kt
@@ -2,21 +2,24 @@ package ru.devambrosov.searchf2
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import ru.devambrosov.searchf2.databinding.FilmItemBinding
 
 //В конструктор класс передается layout, который мы создали(film_item.xml)
 class FilmViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    private val binding = FilmItemBinding.bind(itemView)
     //Привязываем View из layout к переменным
-    private val title = itemView.title
-    private val poster = itemView.poster
-    private val description = itemView.description
+    //private val title = binding.itemView.title
+   // private val poster = binding.itemView.poster
+   // private val description = binding.itemView.description
 
     //В этом методе кладем данные из Film в наши View
     fun bind(film: Film) {
         //Устанавливаем заголовок
-        title.text = film.title
+        binding.title.text = film.title
         //Устанавливаем постер
-        poster.setImageResource(film.poster)
+        binding.poster.setImageResource(film.poster)
         //Устанавливаем описание
-        description.text = film.description
+        binding.description.text = film.description
     }
 }
--- app/src/main/java/ru/devambrosov/searchf2/MainActivity.kt
+++ app/src/main/java/ru/devambrosov/searchf2/MainActivity.kt
@@ -4,33 +4,42 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import ru.devambrosov.searchf2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     private lateinit var filmsAdapter: FilmListRecyclerAdapter
+    val filmsDataBase = listOf(
+        Film("Avengers", R.drawable.avengers1, "Cool blokbaster. The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe."),
+        Film("Back to future", R.drawable.back_to_future, "Good fantastic film. Marty McFly, a 17-year-old high school student, is accidentally sent 30 years into the past in a time-traveling DeLorean invented by his close friend, the maverick scientist Doc Brown."),
+        Film("Fight club", R.drawable.fight_club, "Thriller. An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more."),
+        Film("Joker", R.drawable.joker, "Drum. A mentally troubled stand-up comedian embarks on a downward spiral that leads to the creation of an iconic villain."),
+        Film("The lord of the rings", R.drawable.lor, "Great fantasy. Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring."),
+        Film("One plus one", R.drawable.one_plus_one, "Heart-pearsing drum and comedy"),
+        Film("The dark knight", R.drawable.avengers1, "Blokbaster. When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.")
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
-        val main_recycler = findViewById<RecyclerView>(R.id.main_recycler)
+       // val main_recycler = findViewById<RecyclerView>(R.id.main_recycler)
         setContentView(view)
 
         //находим наш RV
-        main_recycler.apply {
+        binding.mainRecycler.apply {
             //Инициализируем наш адаптер в конструктор передаем анонимно инициализированный интерфейс,
             //оставим его пока пустым, он нам понадобится во второй части задания
             filmsAdapter = FilmListRecyclerAdapter(object : FilmListRecyclerAdapter.OnItemClickListener{
                 override fun click(film: Film) {}
             })
             //Присваиваем адаптер
-            adapter = filmsAdapter
+            binding.mainRecycler.adapter = filmsAdapter
             //Присвои layoutmanager
-            layoutManager = LinearLayoutManager(this@MainActivity)
+            binding.mainRecycler.layoutManager = LinearLayoutManager(this@MainActivity)
             //Применяем декоратор для отступов
             val decorator = TopSpacingItemDecoration(8)
-            addItemDecoration(decorator)
+            binding.mainRecycler.addItemDecoration(decorator)
         }
 //Кладем нашу БД в RV
         filmsAdapter.addItems(filmsDataBase)
@@ -71,15 +80,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    val filmsDataBase = listOf(
-        Film("Avengers", R.drawable.avengers1, "Cool blokbaster. The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe."),
-        Film("Back to future", R.drawable.back_to_future, "Good fantastic film. Marty McFly, a 17-year-old high school student, is accidentally sent 30 years into the past in a time-traveling DeLorean invented by his close friend, the maverick scientist Doc Brown."),
-        Film("Fight club", R.drawable.fight_club, "Thriller. An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more."),
-        Film("Joker", R.drawable.joker, "Drum. A mentally troubled stand-up comedian embarks on a downward spiral that leads to the creation of an iconic villain."),
-        Film("The lord of the rings", R.drawable.lor, "Great fantasy. Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring."),
-        Film("One plus one", R.drawable.one_plus_one, "Heart-pearsing drum and comedy"),
-        Film("The dark knight", R.drawable.avengers1, "Blokbaster. When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.")
-    )
+
 
 
 
